3611a8c97bdc43f8f6fb9aa6c2faf5c43a9a1701
Agathe Lagache <agathe.lagache@viacesi.fr>
2019-02-19 14:05:43 +0100

    Add switch case mechanism

--- Tribunal/translations/translation_en_US.ts
+++ Tribunal/translations/translation_en_US.ts
@@ -2,12 +2,76 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
     <context>
+        <name>behavior_1/behavior.xar:/Get Localized Text</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Par défaut</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Par défaut</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Default</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Default</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
         <message>
             <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Hello</source>
             <comment>Text</comment>
             <translation>Hello</translation>
+        </message>
+        <message>
+            <source>1</source>
+            <comment>Text</comment>
+            <translation type="obsolete">1</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>2</source>
+            <comment>Text</comment>
+            <translation type="obsolete">2</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>1</source>
+            <comment>Text</comment>
+            <translation type="unfinished">1</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>3</source>
+            <comment>Text</comment>
+            <translation type="obsolete">3</translation>
         </message>
     </context>
 </TS>
